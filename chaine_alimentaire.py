ec51a3e50c5ed7f168d4fc468e8bc8d09a0d48db
Terreclair <82955208+TerreBleue@users.noreply.github.com>
2021-12-01 17:31:27 +0100

    prémices des opérations

--- chaine_alimentaire.py
+++ chaine_alimentaire.py
@@ -1,12 +1,23 @@
-def updateChaine(chaine: dict[str]) -> bool:
-    with open("chaine.txt", 'r+') as file:
-        pass
+
+def addPredator(chaine: dict[str, list[str]]) -> bool:
+    with open("chaine.txt", 'r') as file:
+        content = file.read().split("\n")
+    content[1] += " ".join([ani for ani in chaine.keys()])
+    content[0] = str(len(content[1].split(" ")))
+
+    pass  # matrice
+    with open("chaine.txt", 'w') as file:
+        return True
 
 
 def deleteChElement(animal: str) -> bool:
     with open("chaine.txt", 'r') as file:
-        content = file.read()
+        content = file.read().split("\n")
+        ani_index = content[1].split(" ").index(animal)
+
+    pass  # matrice
     with open("chaine.txt", 'w') as filewrite:
         pass
 
+    return True
 
