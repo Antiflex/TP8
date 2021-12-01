3080ff693479901250aadc1e14bfa75e03b29f50
Terreclair <82955208+TerreBleue@users.noreply.github.com>
2021-12-01 18:19:32 +0100

    addPredator

--- chaine_alimentaire.py
+++ chaine_alimentaire.py
@@ -1,22 +1,25 @@
+from function import MatChain
+
 
 def addPredator(chaine: dict[str, list[str]]) -> bool:
+    init_matrice = [[str(el) for el in line] for line in MatChain()]
     with open("chaine.txt", 'r') as file:
         content = file.read().split("\n")
     content[1] += " ".join([ani for ani in chaine.keys()])
     content[0] = str(len(content[1].split(" ")))
 
     animal_list = [animal for animal in content[1].split(" ")]
-    newlines = [[0 for __ in range(int(content[0]))]
-                for pred in chaine.values()]
-    for i, predator in enumerate(chaine.keys()):
-        for ani_index, animal in enumerate(animal_list):
-            if animal in chaine[predator]:
-                newlines[i][ani_index] = 1
-    newcolumns = [[] newlines]
+    init_matrice += [[0 for __ in range(int(content[0]))]
+                     for pred in chaine.values()]
 
+    for ani_index, animal in enumerate(animal_list):
+        for i, predator in enumerate(chaine.keys()):
+            if animal in chaine[predator]:
+                init_matrice[ani_index].append('1')
 
     with open("chaine.txt", 'w') as file:
-        return True
+        file.write("\n".join(content[0:1] +
+                             ([" ".join(line) for line in init_matrice])))
 
 
 def deleteChElement(animal: str) -> bool:
@@ -29,4 +32,3 @@ def deleteChElement(animal: str) -> bool:
         pass
 
     return True
-
