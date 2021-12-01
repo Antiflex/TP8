178f0cc27785c9b11c6cb4651e1000a9bce2fba5
Terreclair <82955208+TerreBleue@users.noreply.github.com>
2021-12-01 17:11:43 +0100

    prémices des opérations

--- chaine_alimentaire.py
+++ chaine_alimentaire.py
@@ -2,4 +2,11 @@ def updateChaine(chaine: dict[str]) -> bool:
     with open("chaine.txt", 'r+') as file:
         pass
 
-def
+
+def deleteChElement(animal: str) -> bool:
+    with open("chaine.txt", 'r') as file:
+        content = file.read()
+    with open("chaine.txt", 'w') as filewrite:
+        pass
+
+
