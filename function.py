3226e8c97df6f888d8f823ee0341abedb4841a41
Antiflex <68072873+Antiflex@users.noreply.github.com>
2021-12-01 17:15:02 +0100

    dico d animaux

--- function.py
+++ function.py
@@ -1,3 +1,13 @@
+def ReadAnimalsInChaine():
+    with open("chaine.txt", "r") as chaine:
+        ListMatrixChaine = chaine.read().split("\n")
+        ListAnimals = ListMatrixChaine[1].split(" ")
+    D1, D2 = {}, {}
+    for i, Name in enumerate(ListAnimals):
+        D1[str(Name)] = i
+        D2[i] = str(Name)
+    return D1, D2
+
 def MatChain(filename:str="chaine.txt")->list[list[int]]:
     with open("chaine.txt","r") as c :
         lines=c.read().split("\n")
